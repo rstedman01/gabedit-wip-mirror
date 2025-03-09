07e213e157af13112b758e555d5bc4a2cab424f2
halfbakedscripts <ryan@GU604.hsd1.co.comcast.net>
2025-03-09 06:49:19 -0600

    Initial commit

---
+++ src/Files/FolderChooser.h
@@ -0,0 +1,7 @@
+
+#ifndef __GABEDIT_FOLDERCHOOSER_H__
+#define __GABEDIT_FOLDERCHOOSER_H__
+
+GtkWidget* selectionOfDir(gpointer data, gchar* title, GabEditTypeWin typewin) ;
+
+#endif /* __GABEDIT_FOLDERCHOOSER_H__ */
